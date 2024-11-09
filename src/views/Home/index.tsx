06128aeabfb96646dd9cb1ee8d75b39ca1260b70
Phoenix7290 <b5261064@gmail.com>
2024-11-09 18:52:56 -0300

    Changes in Home and Data

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Header, Footer } from "../../layout";
 import video from "../../assets/video/gym.mp4";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -14,24 +15,36 @@ const Home = () => {
                     muted
                 />
                 <div className="relative z-10 flex flex-col items-center p-4 bg-white bg-opacity-75 text-black dark:bg-gray-900 dark:bg-opacity-75 dark:text-white">
-                    <h1 className="text-4xl font-bold text-center mb-8">Bem-vindo à FJ Personal Gym</h1>
+                    <h1 className="text-4xl font-bold text-center mb-8">Bem-vindo à FJ Personal CT</h1>
                 </div>
             </div>
-            <div className="flex flex-wrap justify-center gap-8 p-4 bg-white text-black dark:bg-gray-900 dark:text-white">
-                <div className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg flex-1 min-w-[300px] max-w-[400px]">
-                    <h2 className="text-2xl font-semibold mb-4">Nossos Serviços</h2>
-                    <ul className="list-disc list-inside">
-                        <li>Treinamento Personalizado</li>
-                        <li>Aulas em Grupo</li>
-                        <li>Planos de Nutrição</li>
-                        <li>Coaching Online</li>
-                    </ul>
+            <article className="flex flex-col justify-center bg-white text-black dark:bg-gray-900 dark:text-white py-10">
+                <h2 className="text-4xl font-bold text-center mb-8">Veja mais:</h2>
+                <div className="flex flex-wrap justify-center gap-8 p-4 bg-white text-black dark:bg-gray-900 dark:text-white">
+                    <section className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg flex-1 min-w-[300px] max-w-[400px] md:max-w-[600px]">
+                        <h3 className="text-2xl font-semibold mb-4">Nossos Serviços</h3>
+                        <ul className="list-disc list-inside">
+                            <li>Treinamento Personalizado</li>
+                            <li>Aulas em Grupo</li>
+                            <li>Planos de Nutrição</li>
+                            <li>Coaching Online</li>
+                        </ul>
+                    </section>
+                    <section className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg flex-1 min-w-[300px] max-w-[400px] md:max-w-[600px]">
+                        <h3 className="text-2xl font-semibold mb-4">Por Que Nos Escolher?</h3>
+                        <p>No FJ Personal CT, estamos dedicados a ajudar você a alcançar seus objetivos de fitness. Nossos treinadores experientes e programas abrangentes são projetados para atender a todos os níveis de condicionamento físico.</p>
+                    </section>
+                    <section className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg flex-1 min-w-[300px] max-w-[400px] md:max-w-[600px]">
+                        <h3 className="text-2xl font-semibold mb-4">Nossos Personais:</h3>
+                        <Link to="/personal" className="text-blue-500">Conheça nossos personais</Link>
+                    </section>
+                    <section className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg flex-1 min-w-[300px] max-w-[400px] md:max-w-[600px]">
+                        <h3 className="text-2xl font-semibold mb-4">Mais informações? Nossos contatos</h3>
+                        <p>Telefone: (11) 9999-9999</p>
+                        <p>Email: generic@gmail.com</p>
+                    </section>
                 </div>
-                <div className="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-lg flex-1 min-w-[300px] max-w-[400px]">
-                    <h2 className="text-2xl font-semibold mb-4">Por Que Nos Escolher?</h2>
-                    <p>No FJ Personal Gym, estamos dedicados a ajudar você a alcançar seus objetivos de fitness. Nossos treinadores experientes e programas abrangentes são projetados para atender a todos os níveis de condicionamento físico.</p>
-                </div>
-            </div>
+            </article>
             <Footer />
         </>
     );
